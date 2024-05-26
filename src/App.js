18bd83653428588ff95803ba829ad3a9885c293b
Ajay Veer Singh <avisingh.veer@gmail.com>
2024-05-26 23:36:28 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
+  const [firstName, setFirstName]= useState("")
+  const [lastName, setLastName]= useState("")
+  const [fullName, setFullName]= useState("")
+
+  function HandleSubmit(e){
+    e.preventDefault()
+    if(firstName !==" " && lastName !== "")
+    setFullName(`${firstName} ${lastName}`)
+  else{
+    setFullName(" ")
+    alert("Fill Both feilds")
+  }
+    
+  }
+  function DisplayName(fullName){
+    return fullName? <p>Full Name: {fullName}</p>:null
+  }
+   
   return (
     <div className="App">
-      <form><h1>Full Name Display</h1>
-      <p>First Name: <input placeholder="Enter First Name" /></p>
-      <p>Last Name: <input placeholder="Enter Last Name" /></p>
+      <form onSubmit={HandleSubmit}><h1>Full Name Display</h1>
+      
+      <p>First Name: <input value={firstName} placeholder="Enter First Name" onChange={(e)=>setFirstName(e.target.value)} /></p>
+      <p>Last Name: <input value={lastName} placeholder="Enter Last Name" onChange={(e)=>setLastName(e.target.value)}  /></p>
       <button type='submit'>Submit</button>
-      <p>Full Name: {} {}</p>
-
-            
+      {DisplayName(fullName)}
       </form>
 
     </div>
